60c04bbb1246976bd34e0ec8830e1f5d829b81d2
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-16 23:26:06 +0600

    this is navber design

---
+++ src/Components/Navber.jsx
@@ -0,0 +1,79 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Navber = () => {
+    const pathName = usePathname()
+    return (
+        <div className="navbar">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <Link href={'/'} className="btn btn-ghost text-xl"><span className='text-[#EE6C0C]'>GYM</span>FIT</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        navLinks.map(link => <li className={`${pathName === link.path && 'border-b-2 border-[#EE6C0C]'} font-bold uppercase`} key={link.path}><Link href={link.path}>{link.title}</Link></li>)
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+const navLinks = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'About',
+        path: '/about'
+    },
+    {
+        title: 'Courses',
+        path: '/courses'
+    },
+    {
+        title: 'Trainers',
+        path: '/trainers'
+    },
+    {
+        title: 'Contacts',
+        path: '/contacts'
+    },
+]
+
+export default Navber;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import Navber from "../Components/Navber";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+    <div className="min-h-screen text-white font-[family-name:var(--font-geist-sans)]">
+      <main className="font-serif">
+        <header className="bg-[url('/Banner.jpg')] bg-no-repeat bg-cover">
+          <Navber></Navber>
+        </header>
       </main>
     </div>
   );
