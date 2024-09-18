85b88e5bb9add3fdd7608fed9da7d9dea53697a9
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-18 23:10:38 +0600

    run section responsive okay

--- src/Components/Run.jsx
+++ src/Components/Run.jsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 const Run = () => {
     return (
-        <div className="bg-black rounded-xl p-10 mt-16 flex items-center justify-between">
+        <div className="bg-black rounded-xl p-5 md:p-10 mt-16 gap-9 flex flex-col md:flex-row items-center justify-between">
             <div className="">
-                <h1 className="text-3xl font-extrabold pb-2">Run an Extra <br />
+                <h1 className="lg:text-3xl text-2xl font-extrabold pb-2">Run an Extra <br />
                     Mile Easily</h1>
-                <p className="w-[473px]">We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts.</p>
+                <p className="lg:w-[473px] md:w-[303px]">We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts.</p>
                 <Link href='/courses' className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase"> Read MORE</Link>
             </div>
             <div>
-                <Image src={'/run.jpg'} className="rounded-2xl" width={400} height={350} alt="run" />
+                <Image src={'/run.jpg'} className="rounded-2xl w-full" width={400} height={350} alt="run" />
             </div>
         </div>
     );
