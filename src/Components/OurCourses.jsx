28e1ced9aadc6e9f52fb99fb265a9f677c05bc9a
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-18 21:10:08 +0600

    our courses section responsive is okay

--- src/Components/OurCourses.jsx
+++ src/Components/OurCourses.jsx
@@ -10,9 +10,9 @@ const OurCourses = () => {
                 <h4 className="font-medium text-xl">Pro Selected</h4>
                 <h2 className="font-bold uppercase text-[#EE6C0C] text-3xl">Our Courses</h2>
             </div>
-            <div className="pt-7 grid grid-cols-1  md:grid-cols-3 gap-6">
+            <div className="pt-7 grid grid-cols-1  md:grid-cols-3 gap-8">
                 {
-                    topCources?.map(course => <div key={course.name} className="text-center">
+                    topCources?.map(course => <div key={course.name} className="text-center pt-2">
                         <div className="flex items-center justify-center">
                             <Image className="rounded-full" src={course.image} width={230} height={230} alt={course.name} />
                         </div>
