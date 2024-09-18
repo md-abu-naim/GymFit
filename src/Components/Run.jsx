a7d9235434ae18f3aecd02b4d81e5f498f3d3e07
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-18 23:03:55 +0600

    run section design is okay

--- src/Components/Run.jsx
+++ src/Components/Run.jsx
@@ -1,16 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Run = () => {
     return (
-        <div className="bg-black">
-            <div>
-                <h1>Run an Extra
+        <div className="bg-black rounded-xl p-10 mt-16 flex items-center justify-between">
+            <div className="">
+                <h1 className="text-3xl font-extrabold pb-2">Run an Extra <br />
                     Mile Easily</h1>
-                <p>We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts.</p>
+                <p className="w-[473px]">We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts.</p>
                 <Link href='/courses' className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase"> Read MORE</Link>
             </div>
             <div>
-
+                <Image src={'/run.jpg'} className="rounded-2xl" width={400} height={350} alt="run" />
             </div>
         </div>
     );
