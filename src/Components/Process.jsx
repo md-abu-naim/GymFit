4495601320beb9b85035815ab02e92479fafd2a2
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-19 15:27:45 +0600

    process section design is okay

--- src/Components/Process.jsx
+++ src/Components/Process.jsx
@@ -2,17 +2,35 @@ import Image from "next/image";
 
 const Process = () => {
     return (
-        <div>
+        <div className="py-20 px-5 lg:px-20">
             <div className="border-l-2 border-[#EE6C0C] pl-6">
                 <h4 className="font-medium text-xl">Awsome Process</h4>
                 <h2 className="font-bold uppercase text-[#EE6C0C] text-3xl">Our Process</h2>
             </div>
-            <div className="grid md:grid-cols-3 grid-cols-1 gap-7">
-                <div>
-                    <div className="bg-[#EE6C0C] rounded-full w-32 h-32 flex items-center justify-center">
-                        <Image src={'/icon1.png'} width={50} height={50} alt="icon" />
+            <div className="grid pt-6 md:grid-cols-4 grid-cols-1 gap-7">
+                <div className="flex items-center justify-center flex-col">
+                    <div className="bg-[#EE6C0C] border-[8px] hover:border-4 hover:p-3 border-[#8c3e07] rounded-full w-36 h-36 flex items-center justify-center">
+                        <Image src={'/icon1.png'} width={55} height={50} alt="icon" />
                     </div>
-                    <h3>Analyse your Goal</h3>
+                    <h3 className="pt-3 text-2xl hover:text-[#EE6C0C] font-semibold">Analyse your Goal</h3>
+                </div>
+                <div className="flex items-center justify-center flex-col">
+                    <div className="bg-[#EE6C0C] border-[8px] hover:border-4 hover:p-3 border-[#8c3e07] rounded-full w-36 h-36 flex items-center justify-center">
+                        <Image src={'/icon2.png'} width={60} height={50} alt="icon" />
+                    </div>
+                    <h3 className="pt-3 text-2xl hover:text-[#EE6C0C] font-semibold">Analyse your Goal</h3>
+                </div>
+                <div className="flex items-center justify-center flex-col">
+                    <div className="bg-[#EE6C0C] border-[8px] hover:border-4 hover:p-3 border-[#8c3e07] rounded-full w-36 h-36 flex items-center justify-center">
+                        <Image src={'/icon3.png'} width={80} height={50} alt="icon" />
+                    </div>
+                    <h3 className="pt-3 text-2xl hover:text-[#EE6C0C] font-semibold">Analyse your Goal</h3>
+                </div>
+                <div className="flex items-center justify-center flex-col">
+                    <div className="bg-[#EE6C0C] border-[8px] hover:border-4 hover:p-3 border-[#8c3e07] rounded-full w-36 h-36 flex items-center justify-center">
+                        <Image  src={'/icon4.png'} width={50} height={50} alt="icon" />
+                    </div>
+                    <h3 className="pt-3 text-2xl hover:text-[#EE6C0C] font-semibold">Analyse your Goal</h3>
                 </div>
             </div>
         </div>
