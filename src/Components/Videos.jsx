1547da08d276825be6ccc783f9c771d1a987eb8b
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-19 18:55:11 +0600

    start video section

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 
 const Videos = () => {
     return (
         <div className="bg-[url('/videos.jpg')]">
-            watch now
+            <Image src={'/play.webp'} width={150} height={150} alt="Play" />
         </div>
     );
 };
